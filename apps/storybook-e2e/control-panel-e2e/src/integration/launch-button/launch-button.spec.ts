c3fb00a08f5914fcbbe9ee67889beea5a1850759
Juri <juri.strumpflohner@gmail.com>
2021-07-09 16:35:43 +0200

    add Cypress tests for button

--- apps/storybook-e2e/control-panel-e2e/src/integration/launch-button/launch-button.spec.ts
+++ apps/storybook-e2e/control-panel-e2e/src/integration/launch-button/launch-button.spec.ts
@@ -1,7 +1,22 @@
 describe('control-panel: LaunchButton component', () => {
-  beforeEach(() => cy.visit('/iframe.html?id=launchbutton--primary&args=onClick;'));
-    
-    it('should render the component', () => {
-      cy.get('h1').should('contain', 'Welcome to control-panel!');
-    });
+  beforeEach(() =>
+    cy.visit('/iframe.html?id=launchbutton--primary&args=onClick;')
+  );
+
+  it('should render the launch button', () => {
+    cy.get('[data-testing="launch-button"]').should('contain.text', 'Launch');
+  });
+
+  it('should change the text & be disabled when clicking the button', () => {
+    cy.get('[data-testing="launch-button"]').click();
+    cy.get('[data-testing="launch-button"]').should(
+      'contain.text',
+      'Launching...'
+    );
+    cy.get('[data-testing="launch-button"]').should(
+      'have.attr',
+      'aria-disabled',
+      'true'
+    );
+  });
 });
--- libs/control-panel/src/lib/launch-button.tsx
+++ libs/control-panel/src/lib/launch-button.tsx
@@ -1,15 +1,30 @@
+import { useState } from 'react';
 import styles from './launch-button.module.scss';
-
-/* eslint-disable-next-line */
 export interface LaunchButtonProps {
-  enabled?: boolean;
-  onClick: (e: unknown) => void;
+  onClick: () => void;
 }
 
-export function LaunchButton(props: LaunchButtonProps) {
+export function LaunchButton({
+  onClick: launchButtonClick,
+}: LaunchButtonProps) {
+  const [isEnabled, setEnabled] = useState(true);
+
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  const onClick = (e: any) => {
+    e.preventDefault();
+
+    isEnabled && launchButtonClick();
+    setEnabled(false);
+  };
+
   return (
-    <div className={styles.button} onClick={props.onClick}>
-      Launch
+    <div
+      className={`${styles.button} ${isEnabled ? '' : styles.disabled}`}
+      onClick={onClick}
+      data-testing="launch-button"
+      aria-disabled={!isEnabled}
+    >
+      {isEnabled ? 'Launch' : 'Launching...'}
     </div>
   );
 }
